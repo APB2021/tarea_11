af2c01106550894c46bdb1f5a310799745a9eb9a
alumno <alumno@dam224>
2024-11-14 09:52:39 +0100

    Commit con el menú creado

--- tarea_11/src/main/java/tarea_11/App.java
+++ tarea_11/src/main/java/tarea_11/App.java
@@ -2,8 +2,6 @@ package tarea_11;
 
 public class App {
 	public static void main(String[] args) {
-
 		new Menu().mostrarMenu();
-
 	}
 }
--- tarea_11/src/main/java/tarea_11/Menu.java
+++ tarea_11/src/main/java/tarea_11/Menu.java
@@ -1,16 +1,20 @@
 package tarea_11;
 
+import java.util.Scanner;
+
 public class Menu {
+
+	public static Scanner sc = new Scanner(System.in);
 
 	public void mostrarMenu() {
 		int opcion = -1;
 
-		while (true) {
+		while (opcion != 10) {
 			System.out.println(
 					"""
-							==================================================
-							 AD - Tarea 11
-							==================================================
+							=============================================================================================================
+							AD - Tarea 11 - MySql
+							=============================================================================================================
 							 1. Insertar un nuevo alumno.
 							 2. Mostar todos los alumnos (en consola).
 							 3. Guardar todos los alumnos en un fichero (tú eliges el formato del fichero, pero no puede ser XML ni JSON).
@@ -21,7 +25,25 @@ public class Menu {
 							 8. Guardar todos los alumnos en un fichero XML o JSON.
 							 9. Leer un fichero XML o JSON de alumnos (con en formato anterior) y guardarlos en la BD.
 							 10. Salir.
+							=============================================================================================================
 														""");
+			System.out.print("Por favor, seleccione una opción (Entre 0 y 10): ");
+			opcion = sc.nextInt();
+
+			switch (opcion) {
+			case 1 -> System.out.println("Ha elegido: Insertar un nuevo alumno.");
+			case 2 -> System.out.println();
+			case 3 -> System.out.println();
+			case 4 -> System.out.println();
+			case 5 -> System.out.println();
+			case 6 -> System.out.println();
+			case 7 -> System.out.println();
+			case 8 -> System.out.println();
+			case 9 -> System.out.println();
+			case 10 -> System.out.println("Ha elegido SALIR.");
+
+			default -> throw new IllegalArgumentException("Opción incorrecta: " + opcion);
+			}
 		}
 	}
 }
