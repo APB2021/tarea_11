2a28766296f340108018c1c634a02951ab101f93
alumno <alumno@dam224>
2024-11-14 09:10:13 +0100

    Primer commit tarea_11 en clase

---
+++ tarea_11/src/test/java/tarea_11/tarea_11/AppTest.java
@@ -0,0 +1,19 @@
+package tarea_11.tarea_11;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
